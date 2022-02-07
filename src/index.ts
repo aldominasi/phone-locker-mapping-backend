4414c6657285af448913fd4b7ad6d07d9fcb1387
aldominasi <aldo.minasi@gmail.com>
2022-02-07 20:20:58 +0100

    fix lint on index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,20 @@ import { fastify, FastifyRequest, FastifyReply } from 'fastify';
 const PORT: number | string = process.env.PORT ?? 3000;
 
 const server = fastify({
-    logger: true
+  logger: true
 });
 
 const start = async (): Promise<void> => {
-    try {
-        // Test index
-        server.get('/', async (request: FastifyRequest, reply: FastifyReply): Promise<void> => {
-            return reply.code(200).send("Hello World");
-        });
-        await server.listen(PORT, '0.0.0.0');
-    } catch (ex) {
-      server.log.error(ex);
-      process.exit(1);
-    }
-}
+  try {
+    // Test index
+    server.get('/', async (request: FastifyRequest, reply: FastifyReply): Promise<void> => {
+      return reply.code(200).send("Hello World");
+    });
+    await server.listen(PORT, '0.0.0.0');
+  } catch (ex) {
+    server.log.error(ex);
+    process.exit(1);
+  }
+};
 
 void start();
