d7274e5164f1faa0dd995af2405f6bf7896116da
aldominasi <aldo.minasi@gmail.com>
2022-02-13 14:22:18 +0100

    creato lo schema per i ruoli

---
+++ src/entities/ruoli/ruoli.interface.ts
@@ -0,0 +1,10 @@
+import { Document } from 'mongoose';
+
+export default interface IRuoli extends Document {
+  descrizione: string;
+  visualizzaArmadi: boolean;
+  modificaArmadi: boolean;
+  creaArmadi: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
---
+++ src/entities/ruoli/ruoli.schema.ts
@@ -0,0 +1,25 @@
+import { model, Schema } from 'mongoose';
+import IRuoli from './ruoli.interface';
+
+const ruoliSchema: Schema = new Schema<IRuoli>({
+  descrizione: {
+    type: String,
+    required: true
+  },
+  visualizzaArmadi: {
+    type: Boolean,
+    default: false
+  },
+  modificaArmadi: {
+    type: Boolean,
+    default: false
+  },
+  creaArmadi: {
+    type: Boolean,
+    default: false
+  }
+}, {
+  timestamps: true
+});
+
+export default model<IRuoli>('ruoli', ruoliSchema);
