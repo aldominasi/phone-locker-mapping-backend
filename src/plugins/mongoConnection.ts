fe188e79f2bf9cb53e0abb06678ddf7f4aba6f42
aldominasi <aldo.minasi@gmail.com>
2022-02-23 18:51:46 +0100

    eliminato un commento

--- src/plugins/mongoConnection.ts
+++ src/plugins/mongoConnection.ts
@@ -16,7 +16,6 @@ const connectToDb: FastifyPluginAsync = async function (server: FastifyInstance)
       });
     }
     else {
-      //console.log(`mongodb://${process.env.DB_USER}:${process.env.DB_PWD}@${process.env.HOST_DB}/${process.env.DB_NAME}?authSource=admin`);
       await mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PWD}@${process.env.MONGO_DB_HOST}/${process.env.DB_NAME}?authSource=admin`, {
         autoIndex: true
       });
