e89e317ab9d6d7529f6d9fa5c1c59328beeb1d11
aldominasi <aldo.minasi@gmail.com>
2022-03-25 12:57:21 +0100

    Bug fix response codError in serialization ResponseApi

--- src/schemas/serializations/responseApi.serialization.ts
+++ src/schemas/serializations/responseApi.serialization.ts
@@ -3,4 +3,4 @@ import S from 'fluent-json-schema';
 export default S.object()
   .prop('success', S.boolean())
   .prop('msg', S.string())
-  .prop('codError', S.integer().raw({ nullable: true }));
+  .prop('codError', S.string().raw({ nullable: true }));
