30bdc4ef3f0ae1d38961c18bb52edbe39fad34b0
aldominasi <aldo.minasi@gmail.com>
2022-04-26 20:49:17 +0200

    Eliminato il pattern per oldPwd in modificaPwd.validation.ts

--- src/schemas/validations/modificaPwd.validation.ts
+++ src/schemas/validations/modificaPwd.validation.ts
@@ -2,7 +2,7 @@ import S from 'fluent-json-schema';
 const regexPassword = /^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9]).{6,})\S$/;
 
 export const bodyVal = S.object()
-  .prop('oldPwd', S.string().pattern(regexPassword).required())
+  .prop('oldPwd', S.string().required())
   .prop('newPwd', S.string().pattern(regexPassword).required());
 
 export const queryVal = S.object()
