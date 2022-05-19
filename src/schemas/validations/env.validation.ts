8acf3f12491539059b6a167ef640a7edadb12e1a
aldominasi <aldo.minasi@gmail.com>
2022-05-19 18:47:25 +0200

    Aggiornata la validazione dell'environment

--- src/schemas/validations/env.validation.ts
+++ src/schemas/validations/env.validation.ts
@@ -1,9 +1,17 @@
 import S from 'fluent-json-schema';
 
 export default S.object()
-  .prop('MONGO_INITDB_ROOT_USERNAME', S.string().required())
-  .prop('MONGO_INITDB_ROOT_PASSWORD', S.string().required())
+  .prop('MONGO_INITDB_ROOT_USERNAME', S.string())
+  .prop('MONGO_INITDB_ROOT_PASSWORD', S.string())
   .prop('MONGO_INITDB_DATABASE', S.string().required())
   .prop('PORT_PLM_BACKEND', S.integer())
   .prop('SALT_PWD', S.integer())
-  .prop('SEED_JWT_TOKEN', S.string().required());
+  .prop('SEED_JWT_TOKEN', S.string().required())
+  .allOf([
+    S.ifThen(S.object().prop('DEVELOPMENT', S.anyOf([S.string().maxLength(0), S.null()])),
+      S.object()
+        .prop('MONGO_INITDB_ROOT_USERNAME', S.string().required())
+        .prop('MONGO_INITDB_ROOT_PASSWORD', S.string().required())
+    ),
+  ]);
+
