c96a6527522c6204c54631a9f37c6896f7be850e
aldominasi <aldo.minasi@gmail.com>
2022-05-23 18:34:17 +0200

    Aggiornata la validazione dell'env

--- src/schemas/validations/env.validation.ts
+++ src/schemas/validations/env.validation.ts
@@ -3,6 +3,8 @@ import S from 'fluent-json-schema';
 export default S.object()
   .prop('MONGO_INITDB_ROOT_USERNAME', S.string())
   .prop('MONGO_INITDB_ROOT_PASSWORD', S.string())
+  .prop('AUTH_DB', S.string())
+  .prop('HOST_PLM', S.string())
   .prop('MONGO_INITDB_DATABASE', S.string().required())
   .prop('PORT_PLM_BACKEND', S.integer())
   .prop('SALT_PWD', S.integer())
@@ -12,6 +14,8 @@ export default S.object()
       S.object()
         .prop('MONGO_INITDB_ROOT_USERNAME', S.string().required())
         .prop('MONGO_INITDB_ROOT_PASSWORD', S.string().required())
+        .prop('AUTH_DB', S.string().required())
+        .prop('HOST_PLM', S.string().required())
     ),
   ]);
 
